c2e98e72ab5ed27f59da28c4e0a6202016c0ef73
Juan José Martinez <juanjomm71@gmail.com>
2022-09-22 21:43:22 -0300

    homework finished

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,24 +1,74 @@
 var http = require('http');
-var fs   = require('fs');
+var fs = require('fs');
 
-var beatles=[{
+var beatles = [{
   name: "John Lennon",
   birthdate: "09/10/1940",
-  profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
+  profilePic: "https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
 },
 {
   name: "Paul McCartney",
   birthdate: "18/06/1942",
-  profilePic:"http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
+  profilePic: "http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
 },
 {
   name: "George Harrison",
   birthdate: "25/02/1946",
-  profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
+  profilePic: "https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
 },
 {
   name: "Richard Starkey",
   birthdate: "07/08/1940",
-  profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
+  profilePic: "http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
-]
+];
+
+http.createServer(function (req, res) {
+  if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    return res.end(JSON.stringify(beatles));
+  }
+  if (req.url.substring(0, 5) === '/api/') {
+    const beatle = req.url.split('/').pop(); //Lo que está separado por / se guarda en un arreglo y con pop saco el último elemento
+    const name = beatles.find(beat => encodeURI(beat.name) === beatle);
+    if (name) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      return res.end(JSON.stringify(name));
+    } else {
+      res.writeHead(404);
+      return res.end();
+    };
+  };
+  if (req.url === '/') {
+    fs.readFile('./index.html', function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        return res.end(data);
+      };
+    });
+  };
+  if (req.url.length > 1) {
+    const beatle = req.url.split('/').pop();
+    const name = beatles.find(beat => encodeURI(beat.name) === beatle);
+    if (name) {
+      fs.readFile('./beatle.html', 'utf8', function (err, data) {
+        if (err) {
+          res.writeHead(404);
+          return res.end();
+        } else {
+          data = data.replace('{name}', name.name);
+          data = data.replace('{birthdate}', name.birthdate);
+          data = data.replace('{profilePic}', name.profilePic);
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          return res.end(data);
+        }
+      })
+    } else {
+      res.writeHead(404);
+      return res.end();
+    }
+  }
+}).listen(1337, '127.0.0.1');
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,14 @@
-var fs  = require("fs")
-var http  = require("http")
-
+var fs = require("fs")
+var http = require("http")
 // Escribí acá tu servidor
+http.createServer(function (req, res) {
+    fs.readFile(`./images${req.url}.jpg`, function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end();
+        } else {
+            res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            res.end(data);
+        };
+    });
+}).listen(1337, '127.0.0.1');
